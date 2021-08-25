41b72b2761c28aa564ac939aa790eb6cd906a88f
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-25 16:11:42 +0700

    Add files via upload

---
+++ farm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:4444
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Fammer
+
+cd "$(dirname "$0")"
+
+chmod +x ./farm && sudo ./farm --algo ETHASH --pool $POOL --user $WALLET  $@
